377e71b58661d3b66b71741b11082070d36d7404
ami <example@gmail.com>
2026-08-26 01:51:32 +0530

    refactor(crewai): remove duplicate top-level kickoff execution in agent.py

--- crewai_projects/pro1_flaky_testcase_locator_agent/agent/agent.py
+++ crewai_projects/pro1_flaky_testcase_locator_agent/agent/agent.py
@@ -151,20 +151,3 @@ crew = Crew(
     verbose=True,
     memory=False,
 )
-
-# Input data for CrewAI
-
-inputs = {
-    "jira_1": "SU-10",
-    "jira_2": "SU-11",
-    "file_1": "result1.json",
-    "file_2": "result2.json",
-}
-
-output = crew.kickoff(inputs=inputs)
-token_usage = output.token_usage
-print("--------------Flaky Test Analysis Report-----------------")
-print(f"Prompt Tokens: {token_usage.prompt_tokens}")
-print(f"Completion Tokens: {token_usage.completion_tokens}")
-print(f"Total Tokens:  {token_usage.total_tokens}")
-print(output)
